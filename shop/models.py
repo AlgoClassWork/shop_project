1e4c0474944dc6d0000086698608981fe8e5f68b
AlgoClassWork <kyxec.play@gmail.com>
2025-07-23 13:14:39 +0600

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -7,3 +7,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(related_name='products', on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=100, verbose_name='Название товара')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='URL')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, verbose_name='Изображение')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    stock = models.PositiveIntegerField(verbose_name='Остаток на складе')
+    available = models.BooleanField(default=True, verbose_name='Доступен')
+
+    def __str__(self):
+        return self.name
