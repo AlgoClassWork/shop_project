3ed812c48c2fc823a75b187edd59916da846cb0a
AlgoClassWork <kyxec.play@gmail.com>
2025-07-23 13:45:06 +0600

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Product
 # Create your views here.
+#http://127.0.0.1:8000/
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
