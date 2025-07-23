a9a9835fb3a40f805992b59345b90b8dd2f78741
AlgoClassWork <kyxec.play@gmail.com>
2025-07-23 13:51:27 +0600

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -4,5 +4,6 @@ from .models import Product
 # Create your views here.
 #http://127.0.0.1:8000/
 def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'product_list.html', {'products': products})
+    products = Product.objects.filter(available=True)
+    context = {'products': products}
+    return render(request, 'product_list.html', context)
