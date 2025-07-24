3a5e58603cde3ce175832aa73be5079e784d15ad
AlgoClassWork <kyxec.play@gmail.com>
2025-07-24 12:20:44 +0600

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
-from .models import Product
+from .models import Product, Category
 # Create your views here.
 #http://127.0.0.1:8000/
 def product_list(request):
     products = Product.objects.filter(available=True)
-    context = {'products': products}
+    #http://127.0.0.1:8000/category/elektronika
+    categories = Category.objects.all()
+    context = {'products': products, 'categories': categories}
     return render(request, 'product_list.html', context)
